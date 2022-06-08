3dcd4ee42993b5a23822e81a54ed16922e19800a
Lukas Hajda <lukas.hajda@demi.sk>
2022-06-08 18:48:38 +0200

    Basics initializations (master)

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,39 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/assets/sass/style.scss', 'public/assets')
+    .sass('resources/assets/sass/login.scss', 'public/assets')
+
+    // ******************** Web frontend ********************
+    .styles([
+        'resources/assets/frontend/css/bootstrap.min.css',
+
+        'public/assets/style.css'
+    ], 'public/css/style.min.css')
+
+    .scripts([
+        'resources/assets/frontend/js/jquery.min.js',
+        'resources/assets/frontend/js/bootstrap.bundle.min.js',
+
+        'resources/assets/js/frontend.js'
+    ], 'public/js/script.min.js')
+
+
+    // ******************** Login ********************
+    .styles([
+        'resources/assets/frontend/css/bootstrap.min.css',
+
+        'public/assets/login.css'
+    ], 'public/css/login.min.css')
+
+    .scripts([
+        'resources/assets/frontend/js/jquery.min.js',
+        'resources/assets/frontend/js/bootstrap.bundle.min.js',
+
+        'resources/assets/js/login.js'
+    ], 'public/js/login.min.js')
