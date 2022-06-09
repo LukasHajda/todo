40c51cf10051c76e3a4eeeab29bebe342be9e545
Lukas Hajda <lukas.hajda@demi.sk>
2022-06-09 13:23:27 +0200

    Add item categories seeder (master)

--- database/seeders/ItemCategoriesSeeder.php
+++ database/seeders/ItemCategoriesSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\ItemCategory;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemCategoriesSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class ItemCategoriesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $priorities = ['low', 'medium', 'high', 'urgent'];
+        foreach ($priorities as $priority) {
+            if (!ItemCategory::where('name', $priority)->count() > 0) {
+                DB::table('item_categories')->insert([
+                    'name' => $priority,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ]);
+            }
+        }
     }
 }
